e588e3f5309ee7bddb2ac771797c3ec921547a24
Maxime Jingeaux <maxime.jingeaux@ecole.ensicaen.fr>
2023-10-08 16:50:50 +0200

    Ajout d'un second joueur et display noms sur le plateau

--- src/main/java/fr/ensicaen/ecole/genielogiciel/model/Model.java
+++ src/main/java/fr/ensicaen/ecole/genielogiciel/model/Model.java
@@ -1,13 +1,17 @@
 package fr.ensicaen.ecole.genielogiciel.model;
 
 public class Model {
-    private String _nickname;
+    private String _nickname1;
 
-    public String getNickname() {
-        return _nickname;
-    }
+    private String _nickname2;
 
-    public void setNickname( String nickname ) {
-        _nickname = nickname;
+    public String getNickname1() {
+        return _nickname1;
     }
+    public String getNickname2() {return _nickname2;}
+
+    public void setNickname1( String nickname ) {
+        _nickname1 = nickname;
+    }
+    public void setNickname2( String nickname ) {_nickname2 = nickname;}
 }
--- src/main/java/fr/ensicaen/ecole/genielogiciel/view/GameView.java
+++ src/main/java/fr/ensicaen/ecole/genielogiciel/view/GameView.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -23,8 +24,10 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Popup;
+import javafx.stage.PopupWindow;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -39,11 +42,15 @@ public final class GameView {
     @FXML
     private GridPane grid_anchor;
 
+    private String nickName1;
+
+    private String nickName2;
+
     private static final int MAX_LENGTH = 8;
     private static final int MAX_HEIGHT = 8;
     private int index = 0;
 
-    public static GameView createView() throws IOException {
+    public static GameView createView(String nickName) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(GameView.class.getResource("Board.fxml"));
         Parent root = fxmlLoader.load();
@@ -53,14 +60,35 @@ public final class GameView {
         Stage stage = new Stage();
         stage.setScene(scene);
         view._stage = stage;
+        Text player_name = new Text(nickName);
+        view.container.getChildren().add(player_name);
         scene.addEventHandler(KeyEvent.KEY_PRESSED, event -> view.onKeyPressed(event.getCode()));
-
         return view;
     }
 
+    public static GameView createView(String nickName1,String nickName2) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(GameView.class.getResource("Board.fxml"));
+        Parent root = fxmlLoader.load();
+        final GameView view = fxmlLoader.getController();
+        fxmlLoader.setController(view);
+        Scene scene = new Scene(root, 1000, 1000);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        view._stage = stage;
+        Text player_name1 = new Text(nickName1);
+        view.container.getChildren().add(player_name1);
+        Text player_name2 = new Text(nickName2);
+        view.container.getChildren().add(player_name2);
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, event -> view.onKeyPressed(event.getCode()));
+        return view;
+    }
     public void setPresenter( GamePresenter gamePresenter ) {
         _gamePresenter = gamePresenter;
     }
+
+    public void setNickName1(String nickName){nickName1 = nickName;}
+    public void setNickName2(String nickName){nickName2 = nickName;}
 
     public void show() {
         _stage.show();
@@ -75,8 +103,7 @@ public final class GameView {
 
     public void initialize() {
         // Code pour une grille 8*8
-        GamePresenter presenter = new GamePresenter("test");
-        this.setPresenter(presenter);
+        //System.out.println(_gamePresenter);
         grid_anchor.setAlignment(Pos.CENTER);
         container.setAlignment(Pos.CENTER);
         Circle pawn = new Circle(30);
@@ -101,24 +128,11 @@ public final class GameView {
                 container.getChildren().add(stack);
             }
         }
-        /*StackPane stack1 = new StackPane();
-        container.setAlignment(Pos.CENTER);
-        Circle pawn1 = new Circle(25);
-        pawn1.setStyle("-fx-fill:green");
-        Rectangle rect1 = new Rectangle(80,80);
-        rect1.setStyle("-fx-fill: white;-fx-stroke : black");
-        Text text1 = new Text("1");
-        stack1.getChildren().addAll(rect1,text1,pawn1);
-        container.getChildren().add(stack1);
-        StackPane stack2 = new StackPane();
-        Rectangle rect2 = new Rectangle(80,80);
-        rect2.setStyle("-fx-fill: white;-fx-stroke : black");
-        Text text2 = new Text("2");
-        stack2.getChildren().addAll(rect2,text2);
-        container.getChildren().add(stack2);*/
         Button btn = new Button("Lancer");
         Popup popup = new Popup();
         Text text = new Text("Victoire !");
+
+        text.setFont(new Font("Arial",20));
         popup.getContent().add(text);
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -135,28 +149,8 @@ public final class GameView {
                 }
 
             }
-/*
-                if (count == 4){
-
-                    PauseTransition short_delay = new PauseTransition(Duration.seconds(1));
-                    short_delay.setOnFinished(event2->{count++;stack_array[x][y].getChildren().add(pawn);});
-                    short_delay.playFromStart();
-
-                }
-                if (count == 7){
-
-                    if (!popup.isShowing()){
-                        popup.show(_stage);
-                    }
-                    else {
-                        popup.hide();
-                    }
-                }
-            }
-            */
         });
         container.getChildren().add(btn);
-
     }
 
 }
